c0e96f0279b6dbcd34a78b86527c3d349b556c04
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2023-06-05 18:19:24 -0300

    indo por no local storage

--- scripts/infos.js
+++ scripts/infos.js
@@ -2,6 +2,10 @@ function atualizarTamanho(input) {
     input.style.width = ((input.value.length + 1) * 11) + 'px';
   }
 
+  function ajustarAltura(textarea) {
+    textarea.style.height = textarea.scrollHeight + "px";
+  }
+
   function abrirmenuinfo() {
     if (menuInfo.style.display == 'block') {
         menuInfo.style.display = 'none'
