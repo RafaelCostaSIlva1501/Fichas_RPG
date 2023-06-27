35e1f4bef967730d0a0ec3ca1ff1b950bb8f4b6f
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2023-06-26 23:41:48 -0300

    Teste de responsividade com dados feitos e painel de status

--- scripts/status.js
+++ scripts/status.js
@@ -1,53 +1,79 @@
-// Função para atualizar a barra de vida com base nos valores atuais
-function atualizarBarraVida() {
-    const barraVida = document.getElementById('barraProgressoVida');
-    const textoBarraVida = document.getElementById('numeroVida');
-    barraVida.style.width = `${(vidaAtual / vidaTotal) * 100}%`;
-    textoBarraVida.textContent = `${vidaAtual}/${vidaTotal}`;
-  }
+function atualizarValorVida() {
+  var vida = prompt("Valor total de vida");
+  var valorTotal = vida
+  var valorAtual = vida 
+  var barraVida = document.getElementById("barraVidaProgresso")
 
-   
-  
-  // Função para definir a vida total
-  function definirVidaTotal() {
-    const valor = prompt('Qual o valor total de vida que:');
-    const valorNumerico = parseInt(valor);
-    if (!isNaN(valorNumerico) && valorNumerico >= 0) {
-      vidaTotal = valorNumerico;
-      vidaAtual = vidaTotal;
-      atualizarBarraVida();
-    }
+  if (valorTotal !== null) {
+    barraVida.value = valorAtual + "/" + valorTotal;
   }
-  
-  // Função para subtrair vida
-  function subtrairVida() {
-    const valor = prompt('Digite o valor a ser subtraído:');
-    const valorNumerico = parseInt(valor);
-    if (!isNaN(valorNumerico) && valorNumerico > 0) {
-      vidaAtual -= valorNumerico;
-      if (vidaAtual < 0) {
-        vidaAtual = 0;
-      }
-      atualizarBarraVida();
-    }
+}
+
+function subVida() {
+  var sub = prompt("Quanto de vida você perdeu?");
+  var barraVida = document.getElementById("barraVidaProgresso");
+  var valorAtual = parseInt(barraVida.value.split("/")[0]);
+  var valorTotal = parseInt(barraVida.value.split("/")[1]);
+  var novoValor = valorAtual - parseInt(sub);
+
+  if (!isNaN(novoValor)) {
+    barraVida.value = novoValor + "/" + valorTotal;
+  } else{
+    alert("Não é válido")
   }
-  
-  // Função para adicionar vida
-  function adicionarVida() {
-    const valor = prompt('Digite o valor a ser adicionado:');
-    const valorNumerico = parseInt(valor);
-    if (!isNaN(valorNumerico) && valorNumerico > 0) {
-      vidaAtual += valorNumerico;
-      if (vidaAtual > vidaTotal) {
-        vidaAtual = vidaTotal;
-      }
-      atualizarBarraVida();
-    }
+}
+
+function addVida() {
+  var add = prompt("Quanto de vida será adicionado?")
+  var barraVida = document.getElementById("barraVidaProgresso");
+  var valorAtual = parseInt(barraVida.value.split("/")[0]);
+  var valorTotal = parseInt(barraVida.value.split("/")[1]);
+  var novoValor = valorAtual + parseInt(add);
+
+  if (!isNaN(novoValor)) {
+    barraVida.value = novoValor + "/" + valorTotal;
+  } else{
+    alert("Não é válido")
   }
-  
-  // Atribui as chamadas de função aos eventos onclick dos elementos correspondentes
-  
-  document.getElementById('barraVida').onclick = definirVidaTotal;
-  document.getElementById('subVida').onclick = subtrairVida;
-  document.getElementById('addVida').onclick = adicionarVida;
+}
+
+
+function atualizarValorEsforco() {
+  var esforco = prompt("Valor total de esforço");
+  var valorTotalEsforco = esforco;
+  var valorAtualEsforco = esforco;
+  var barraEsforco = document.getElementById("barraEsforcoProgresso");
+
+  if (valorTotalEsforco !== null) {
+    barraEsforco.value = valorAtualEsforco + "/" + valorTotalEsforco;
+  }
+}
+
+function subEsforco() {
+  var sub = prompt("Quanto de esforço você perdeu?");
+  var barraEsforco = document.getElementById("barraEsforcoProgresso");
+  var valorAtualEsforco = parseInt(barraEsforco.value.split("/")[0]);
+  var valorTotalEsforco = parseInt(barraEsforco.value.split("/")[1]);
+  var novoValorEsforco = valorAtualEsforco - parseInt(sub);
+
+  if (!isNaN(novoValorEsforco)) {
+    barraEsforco.value = novoValorEsforco + "/" + valorTotalEsforco;
+  } else {
+    alert("Valor inválido");
+  }
+}
+
+function addEsforco() {
+  var add = prompt("Quanto de esforço será adicionado?");
+  var barraEsforco = document.getElementById("barraEsforcoProgresso");
+  var valorAtualEsforco = parseInt(barraEsforco.value.split("/")[0]);
+  var valorTotalEsforco = parseInt(barraEsforco.value.split("/")[1]);
+  var novoValorEsforco = valorAtualEsforco + parseInt(add);
+
+  if (!isNaN(novoValorEsforco)) {
+    barraEsforco.value = novoValorEsforco + "/" + valorTotalEsforco;
+  } else {
+    alert("Valor inválido");
+  }
+}
 
